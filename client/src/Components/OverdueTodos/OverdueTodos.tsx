f7385110d0feaa461e5f6b1a327400ea382db5c6
spardutti <luisdamian.sp@gmail.com>
2022-02-21 16:35:40 -0300

    removed unusued words

--- client/src/Components/OverdueTodos/OverdueTodos.tsx
+++ client/src/Components/OverdueTodos/OverdueTodos.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Heading, HStack, Text } from "@chakra-ui/react";
+import { Box, Divider, Heading, HStack } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { useGetOverdueTodos } from "../../api/Todo/get_todo";
 import { useAppDispatch, useAppSelector } from "../../hooks";
@@ -20,7 +20,7 @@ const OverdueTodos: React.FC = () => {
       setOverdueTodos(data.data);
       dispatch(setOverdue(data.data.length));
     }
-  }, [data]);
+  }, [data, dispatch]);
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -30,7 +30,7 @@ const OverdueTodos: React.FC = () => {
         <Box p={10}>
           <HStack overflow={"hidden"} justify={"space-between"}>
             <Heading fontSize={15}>Overdue</Heading>
-            <Text color={"red"}>Reschedule</Text>
+            {/* <Text color={"red"}>Reschedule</Text> */}
           </HStack>
           <Divider py={2} />
           {overdueTodos.map((elem, index) => {

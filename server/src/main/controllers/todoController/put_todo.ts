e6ffd618868cc2d92c6c28ac0bee7b8efc09c578
Jorge <jbarretolarrosa@gmail.com>
2022-02-23 00:01:22 -0300

    Working on Edit button function/style. Not done

--- server/src/main/controllers/todoController/put_todo.ts
+++ server/src/main/controllers/todoController/put_todo.ts
@@ -1,6 +1,7 @@
 import { TodoModel } from "../../models/TodoModel";
 import { Request, Response, NextFunction } from "express";
 import { DateTime } from "luxon";
+import { CategoryModel } from "../../models/CategoryModel";
 
 /* EDIT TODO */
 const toggleIsCompleted = async (
@@ -26,5 +27,31 @@ const toggleIsCompleted = async (
     return next(error);
   }
 };
+
+const editTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { todoName, todoDescription, dueDate, categoryId} = req.body;
+    const { id } = req.params;
+    const category = await CategoryModel.findById(categoryId)
+
+    const todo = await TodoModel.findByIdAndUpdate(id, {
+      todoName,
+      todoDescription,
+      dueDate,
+      category,
+      updated: DateTime.now(),
+      updatedType: "Edited",
+    })
+
+    res.status(200).json(todo);
+
+  } catch (error) {
+    return next(error)
+  }
+}
 
 export { toggleIsCompleted };
